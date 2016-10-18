b35a0863c1448c6d3b96c9b57755d8ee248e02ff
tush4r <stushar52@gmail.com>
2016-10-18 13:00:54 +0530

    Font Name bugs

--- CreateImages.py
+++ CreateImages.py
@@ -3,17 +3,20 @@ from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
 
-#Class for Creating Images out of the font file specified
+import GetFontName
+
+# Class for Creating Images out of the font file specified
 class CreateImages(object):
 
     def __init__(self):
         self.fontName = "Arial.ttf"
-    #Function Image from the String Font
+    # Function Image from the String Font
+
     def imageFromFont(self, fontName):
+        valueeee = GetFontName().returnNames()
         font = ImageFont.truetype("Arial.ttf", 14)
         img = Image.new("RGBA", (500, 250), (255, 255, 255))
         draw = ImageDraw.Draw(img)
-        draw.text((0, 0), "This is a test", (0, 0, 0), font=font)
+        draw.text((0, 0), "This is a test", (0, 0, 0), font)
         draw = ImageDraw.Draw(img)
         img.save("a_test.png")
-
---
+++ GetFontName.py
@@ -0,0 +1,7 @@
+import os
+
+class GetFontName():
+
+    def returnNames(self):
+        name_list = os.listdir("/Users/layaksingh/Developer/")
+        return name_list
